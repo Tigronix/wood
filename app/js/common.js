5dae1591e37325261acc3f718d01d0b3e2b04ddf
Vitold <vditold@yandex.ru>
2019-02-25 16:28:26 +0300

    page-header

--- app/js/common.js
+++ app/js/common.js
@@ -1,23 +1,55 @@
 "use strict"
 
 // global object for whole project. DONT change it!
-const RS = {};
+const WD = {};
 
 // global vars
-RS.ESC_CODE = 27;
+WD.ESC_CODE = 27;
 
 // brakepoints
-RS.fromDesktop = window.matchMedia( "(min-width: 1025px)" );
-RS.atMobile = window.matchMedia( "(max-width: 1024px)" );
-RS.less500 = window.matchMedia("(max-width: 500px)");
+WD.fromDesktop = window.matchMedia( "(min-width: 1025px)" );
+WD.atMobile = window.matchMedia( "(max-width: 1024px)" );
+WD.less500 = window.matchMedia("(max-width: 500px)");
 
-RS.svgGlobal = function(){
+WD.svgGlobal = function(){
   new Vue({
     	el: '#global-svg',
-    	template: '<svg aria-hidden="true" style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="icon-menu" viewBox="0 0 32 32"><title>menu</title><path d="M2 6h28v6h-28zM2 14h28v6h-28zM2 22h28v6h-28z"></path></symbol><symbol id="icon-cross" viewBox="0 0 32 32"><title>cross</title><path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path></symbol></defs>'
+    	template: '<svg aria-hidden="true" style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="icon-menu" viewBox="0 0 32 32"><title>menu</title><path d="M2 6h28v6h-28zM2 14h28v6h-28zM2 22h28v6h-28z"></path></symbol><symbol id="icon-cross" viewBox="0 0 32 32"><title>cross</title><path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path></symbol></defs></svg>'
     })
 };
 
+WD.mainMenuToggle = function(){
+  const $menu = $('.js-menu');
+  const $btnOpen = $('.js-menu-open');
+  const $btnClose = $menu.find('.js-menu-close');
+  const $globalWrapper = $('.global-wrapper');
+
+  const popupOpen = function(){
+    $menu.slideDown('500');
+    $globalWrapper.addClass('js-popup-open');
+  };
+
+  const popupClose = function(){
+    $menu.slideUp('500');
+    $globalWrapper.removeClass('js-popup-open');
+  };
+
+  $btnOpen.on('click', function(){
+    popupOpen();
+  });
+
+  $btnClose.on('click', function(){
+    popupClose();
+  });
+
+  $(document).on('keydown.js-main-menu', function onKeyDown(evt) {
+      if (evt.keyCode === WD.ESC_CODE) {
+          popupClose();
+      }
+  });
+};
+
 (function onPageReady () {
-  RS.svgGlobal();
+  WD.svgGlobal();
+  WD.mainMenuToggle();
 }());
